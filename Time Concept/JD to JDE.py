43d11c456e04eeb5652ede64e8044e1dcd343673
ken_21 <alifmaulahaqi@gmail.com>
2025-01-17 00:57:27 +0700

    Update JD to JDE.py

--- Time Concept/JD to JDE.py
+++ Time Concept/JD to JDE.py
@@ -1,1 +1,4 @@
-
+# based on tabel espenak and mees in espenak and mees.md (see in Time concept file)
+TT = int(input("masukan periode waktu sesuai tabel espenak and mees : "))
+YY = int(input("masukan tahun : "))
+U =  ( YY - TT )/ 100
